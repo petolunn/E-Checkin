e8c4cc6e44b3482b20bd736eb443bf779cbb6649
petolunn <54566143+petolunn@users.noreply.github.com>
2020-05-05 22:13:43 +0700

    -

--- db-teacher-home.js
+++ db-teacher-home.js
@@ -93,3 +93,13 @@ function refeshText(){
         console.log("Error getting document:", error);
     });
 }
+
+function makeId(){
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 3; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    document.getElementById("tagId").value = result;
+}
